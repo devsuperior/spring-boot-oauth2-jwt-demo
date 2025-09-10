400bc6d3eed53cec788de4984f1983110d7b2d1a
devdouglasm <devdouglasm@gmail.com>
2025-09-10 04:16:30 -0300

    Spring security

---
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
